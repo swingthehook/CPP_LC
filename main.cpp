e04dd7fb9397b34d3651412863d06f0b12131403
swingthehook <513503877@qq.com>
2023-12-22 23:16:17 +0800

    2023-12-22

--- main.cpp
+++ main.cpp
@@ -4,49 +4,38 @@ using namespace std;
 
 class Solution {
 public:
-    long long maximumSumOfHeights(vector<int>& maxHeights) {
-        int len = maxHeights.size();
-        stack<int> pre,suf;
-        long long presum[len],sufsum[len];
-        for (int i = 0; i < len; ++i) {
-            while(!pre.empty() && maxHeights[i]<maxHeights[pre.top()]){
-                pre.pop();
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int len = temperatures.size();
+        stack<int> st[len];
+        vector<int> ans(len);
+        for (int i = 0; i < len; i++)
+        {
+            while (!st->empty()&&temperatures[i]>temperatures[st->top()]){
+                ans[st->top()] = i-st->top();
+                st->pop();
             }
-            if (pre.empty()){
-                presum[i] = (long long )(i+1)*maxHeights[i];
-            } else{
-                presum[i] = presum[pre.top()] + (long long )(i-pre.top())*maxHeights[i];
-            }
-            pre.push(i);
+            st->push(i);
+            
         }
-        for (int i = len-1; i >=0 ; --i) {
-            while (!suf.empty() && maxHeights[i]<maxHeights[suf.top()]){
-                suf.pop();
-            }
-            if(suf.empty()){
-                sufsum[i] = (long long )(len-i)*maxHeights[i];
-            }
-            else{
-                sufsum[i] = sufsum[suf.top()] + (long long )(suf.top() - i)*maxHeights[i];
-            }
-            suf.push(i);
+        while (!st->empty())
+        {
+            ans[st->top()]=0;
+            st->pop();
         }
-        long long ans=0,temp;
-
-        for (int i = 0; i < len; ++i) {
-            temp = presum[i]+sufsum[i] - maxHeights[i];
-            ans = ans>temp?ans:temp;
-        }
+        
         return ans;
     }
 };
-
 int main() {
 
-    vector<int> maxHeight = {3,5,3,5,1,5,4,4,4};
+    vector<int> maxHeight = {73,74,75,71,69,72,76,73};
     auto sol = new Solution();
-    cout<<sol->maximumSumOfHeights(maxHeight)<<endl;
-
+    auto ans = sol->dailyTemperatures(maxHeight);
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout<<ans[i]<<' ';
+    }
+    
 
 
 
