735e95733ef01fd8a988416fa3f5debaeb552334
swingthehook <513503877@qq.com>
2023-12-27 11:25:23 +0800

    2023.12.27

--- main.cpp
+++ main.cpp
@@ -4,19 +4,24 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> numOfBurgers(int tomatoSlices, int cheeseSlices) {
-        vector<int> ans;
-        if(tomatoSlices > cheeseSlices*4) return ans;
-        if (tomatoSlices < cheeseSlices*2) return ans;
-        if (tomatoSlices%2 == 1) return ans ;
-        ans.push_back((tomatoSlices-2*cheeseSlices)/2);
-        ans.push_back((4*cheeseSlices-tomatoSlices)/2);
-        return ans;
+    int isWinner(vector<int>& player1, vector<int>& player2) {
+        int sum1=0,sum2=0;
+        int len=player1.size();
+        for (int i = 0; i < len; i++)
+        {
+            sum1+= (i>0&&player1[i-1]==10||i>1 && player1[i-2]==10)?player1[i]<<1:player1[i];
+            sum2+= (i>0&&player2[i-1]==10||i>1 &&player2[i-2]==10)?player2[i]<<1:player2[i];
+        }
+        return sum1>sum2?1:(sum1<sum2?2:0);
+        
     }
 };
 
 int main() {
 
-    cout<<"fuck you vscode"<<endl;
+    Solution solution = Solution();
+    vector<int> p1 = {10,2,2,3};
+    vector<int> p2 = {3,8,4,5};
+    cout<<solution.isWinner(p1,p2)<<endl;;
     return 0;
 }
