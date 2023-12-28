2ff6d472dae707fc57cac6d146fcc00a9d173737
swingthehook <513503877@qq.com>
2023-12-28 12:28:31 +0800

    2023.12.28

--- main.cpp
+++ main.cpp
@@ -4,24 +4,26 @@ using namespace std;
 
 class Solution {
 public:
-    int isWinner(vector<int>& player1, vector<int>& player2) {
-        int sum1=0,sum2=0;
-        int len=player1.size();
+    long long minCost(vector<int>& nums, int x) {
+        long long ans=1e9,sum;
+        int len = nums.size();
         for (int i = 0; i < len; i++)
         {
-            sum1+= (i>0&&player1[i-1]==10||i>1 && player1[i-2]==10)?player1[i]<<1:player1[i];
-            sum2+= (i>0&&player2[i-1]==10||i>1 &&player2[i-2]==10)?player2[i]<<1:player2[i];
+            sum=i*x;
+            for (int j = 0; j < len; j++)
+            {
+                sum+=min(nums[j],nums[(j+i)%len]);
+            }
+            ans = sum<ans?sum:ans;
         }
-        return sum1>sum2?1:(sum1<sum2?2:0);
-        
+        return ans;
     }
 };
 
 int main() {
+    
+    vector<int> p = {20,1,5};
+    Solution().minCost(p,5);
 
-    Solution solution = Solution();
-    vector<int> p1 = {10,2,2,3};
-    vector<int> p2 = {3,8,4,5};
-    cout<<solution.isWinner(p1,p2)<<endl;;
     return 0;
 }
