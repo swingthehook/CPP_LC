35fabff1f97e24888b69417a5e138ab475a2b240
swingthehook <513503877@qq.com>
2024-01-25 11:20:57 +0800

    2024.1.25

---
+++ LC2859.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    int sumIndicesWithKSetBits(vector<int> &nums, int k) {
+        int ans = 0, len = nums.size();
+        for (int i = 0; i < len; ++i) {
+            if (__builtin_popcount(i) == k)
+                ans += nums[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {2, 3, 4, 3, 4};
+    auto ans = Solution().alternatingSubarray(nums);
+    cout << ans;
+    return 0;
+}
