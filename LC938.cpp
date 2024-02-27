87d00fb11b82bfa9a95bfc3dc360ce0bfe78d056
swingthehook <513503877@qq.com>
2024-02-27 10:11:56 +0800

    2024.2.26

---
+++ LC938.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root){
+            if(root->val>high)
+                return rangeSumBST(root->left,low,high);
+            else if(root->val < low)
+                return rangeSumBST(root->right,low,high);
+            else
+                return root->val + rangeSumBST(root->left,low,high) + rangeSumBST(root->right,low,high);
+        } else{
+            return 0;
+        }
+    }
+};
+int main() {
+    int num = 2;
+    vector<int> nums = {2, 3, 4, 3, 4};
+    Solution().canMeasureWater(3, 5, 4);
+    return 0;
+}
