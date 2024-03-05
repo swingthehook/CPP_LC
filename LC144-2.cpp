7cf213fa299a710e96def2a5b2e25a833377c65d
swingthehook <513503877@qq.com>
2024-03-05 18:50:18 +0800

    2024.3.5-非递归遍历，放弃中序

---
+++ LC144-2.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> ans;
+        if(!root)
+            return ans;
+        stack<TreeNode *> stk;
+        stk.push(root);
+        while (!stk.empty()) {
+            TreeNode *tmp = stk.top();
+            stk.pop();
+            ans.push_back(tmp->val);
+            if (tmp->right) stk.push(tmp->right);
+            if (tmp->left) stk.push(tmp->left);
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {2, 3, 4, 3, 4};
+    Solution().canMeasureWater(3, 5, 4);
+    return 0;
+}
