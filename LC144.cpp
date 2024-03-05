e9eba0dc2499df69732849d9bcc4cb27cb722a81
swingthehook <513503877@qq.com>
2024-03-05 18:15:06 +0800

    2024.3.5

---
+++ LC144.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+
+int main() {
+    int num = 2;
+    vector<int> nums = {2, 3, 4, 3, 4};
+    Solution().canMeasureWater(3, 5, 4);
+    return 0;
+}
---
+++ LC145.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> ans;
+    void in(TreeNode* root){
+        if(root){
+            in(root->left);
+            in(root->right);
+            ans.push_back(root->val);
+        }
+    }
+    vector<int> postorderTraversal(TreeNode* root) {
+        in(root);
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {2, 3, 4, 3, 4};
+    Solution().canMeasureWater(3, 5, 4);
+    return 0;
+}
