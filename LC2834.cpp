b7b65dd403f9d4b83a943259384b50a8f121acdd
swingthehook <513503877@qq.com>
2024-03-09 18:03:36 +0800

    2024.3.8

---
+++ LC2834.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    //数学计算
+    int minimumPossibleSum(int n, int target) {
+        const int mod = 1e9 + 7;
+        int m = target / 2;
+        if (n <= m) {
+            return (long long) (1 + n) * n / 2 % mod;
+        }
+        return ((long long) (1 + m) * m / 2 +
+                ((long long) target + target + (n - m) - 1) * (n - m) / 2) % mod;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution().findKOr(nums,4);
+    return 0;
+}
