de763cb60c9ac834d78f06bff3aaa731a826579f
swingthehook <513503877@qq.com>
2024-03-14 08:52:56 +0800

    2024.3.14 倒序遍历很重要

---
+++ LC2789.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    long long maxArrayValue(vector<int> &nums) {
+        long long ans = nums.back();
+        for (int i = nums.size() - 2; i >= 0; --i) {
+            ans = nums[i]<=ans?ans+nums[i]:nums[i];
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7, 12, 9, 8, 9, 15};
+    Solution().findKOr(nums, 4);
+    return 0;
+}
