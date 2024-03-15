56ac274c8256b5ee4b9d6336a4806c8180ba76ec
swingthehook <513503877@qq.com>
2024-03-15 10:39:14 +0800

    2024.3.15

--- LC.cpp
+++ LC.cpp
@@ -14,10 +14,40 @@ struct ListNode {
 };
 
 
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+
 
 int main() {
     int num = 2;
-    vector<int> nums = {2, 3, 4, 3, 4};
-    cout << "sfgddxb"<<endl;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution().findKOr(nums,4);
     return 0;
 }
