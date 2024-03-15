1f493f5205e25432fa4c7548a73986f9000ab216
swingthehook <513503877@qq.com>
2024-03-15 10:24:46 +0800

    2024.3.15

---
+++ LC111.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int mindepth=1e9;
+    int minDepth(TreeNode* root) {
+        if(!root)
+            return 0;
+        dfs(root,1);
+        return mindepth;
+    }
+    void dfs(TreeNode *root,int depth){
+        if(!(root->left || root->right) && depth < mindepth){
+            mindepth = depth;
+        }
+        if(root->left)
+            dfs(root->left,depth+1);
+        if(root->right)
+            dfs(root->right,depth+1);
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution().findKOr(nums,4);-
+    return 0;
+}
---
+++ LC226.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root)
+            return root;
+        dfs(root);
+        return root;
+
+    }
+    void dfs(TreeNode *root){
+        swap(root->left,root->right);
+        if(root->left)
+            dfs(root->left);
+        if(root->right)
+            dfs(root->right);
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution().findKOr(nums,4);
+    return 0;
+}
