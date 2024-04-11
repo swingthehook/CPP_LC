7c0fca66e364ede6b26a57833eadcb9d1e20ce56
swingthehook <513503877@qq.com>
2024-04-11 17:07:14 +0800

    2024.4.11

---
+++ LC108.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    TreeNode *rec(vector<int> &nums, int start, int end) {
+        if(start > end)
+            return NULL;
+        int mid = (start + end) / 2;
+        TreeNode *root = new TreeNode(nums[mid]);
+        root->left = rec(nums, start, mid - 1);
+        root->right = rec(nums, mid + 1, end);
+        return root;
+    }
+
+    TreeNode *sortedArrayToBST(vector<int> &nums) {
+        TreeNode *ans = rec(nums, 0, nums.size() - 1);
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7, 12, 9, 8, 9, 15};
+    Solution();
+    return 0;
+}
