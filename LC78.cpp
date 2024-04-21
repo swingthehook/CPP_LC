7979d29f7cbd903b57be9f98e7f075bf695ae2fd
swingthehook <513503877@qq.com>
2024-04-21 16:22:17 +0800

    2024.4.20

---
+++ LC78.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    vector<int> tmp;
+    vector<vector<int>> ans;
+    void rec(vector<int>& nums,int cur){
+        if(cur == nums.size()){
+            ans.push_back(tmp);
+            return;
+        }
+        //不选
+        rec(nums,cur+1);
+        //选
+        tmp.push_back(nums[cur]);
+        rec(nums,cur+1);
+        tmp.pop_back();
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        rec(nums,0);
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution();
+    return 0;
+}
