bd751d52b0a1e2f22b89d8018227a0fb2dbe4e5a
swingthehook <513503877@qq.com>
2024-05-16 12:03:15 +0800

    2024.5.16

---
+++ LC1953.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    long long numberOfWeeks(vector<int>& milestones) {
+        long long ans=0;
+        long long sum=0;
+        int mx=0;
+        for (int i = 0; i < milestones.size(); ++i) {
+            if(milestones[i]>mx)
+                mx=milestones[i];
+            sum+=milestones[i];
+        }
+        if(sum >= (mx<<1))
+            return sum;
+        else return (sum-mx)*2+1;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution();
+    return 0;
+}
