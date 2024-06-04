886a5841f1d81c9c4bec5ef47c0b983fa6103ac7
swingthehook <513503877@qq.com>
2024-06-04 23:57:40 +0800

    2024.6.2

---
+++ LC575.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        int ans=0;
+        unordered_set<int> st;
+        for(auto& each: candyType){
+            if(st.find(each) == st.end()){
+                ans++;
+                st.emplace(each);
+            }
+        }
+        return min(ans,(int)candyType.size()/2);
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution();
+    return 0;
+}
