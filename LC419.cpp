fe7c0586b1608bd9f32140ef858a05c25f8f96d4
swingthehook <513503877@qq.com>
2024-06-11 13:01:51 +0800

    2024.6.11

---
+++ LC419.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int countBattleships(vector<vector<char>> &board) {
+        int m = board.size(), n = board[0].size();
+        int ans = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (board[i][j] == 'X') {
+                    if (i == 0 || i > 0 && board[i - 1][j] != 'X') {
+                        while (j<n && board[i][j] == 'X') ++j;
+                    }else{
+                        continue;
+                    }
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7, 12, 9, 8, 9, 15};
+    Solution();
+    return 0;
+}
