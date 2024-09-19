743f32a6608fa54c203e04904761d2ce27ad72bd
swingthehook <513503877@qq.com>
2024-09-19 13:45:47 +0800

    2024.9.19

---
+++ LC2414.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int longestContinuousSubstring(string s) {
+        int ans = 1;
+        int cnt = 1, i = 0;
+        while (i < s.size()) {
+            if (s[i]+1 == s[i+1]){
+                cnt++;
+            }else{
+                ans = max(ans,cnt);
+                cnt=1;
+            }
+            ++i;
+        }
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7, 12, 9, 8, 9, 15};
+    Solution();
+    return 0;
+}
