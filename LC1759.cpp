dc0aaa90b942307d9535226b972d7717d104bfad
swingthehook <513503877@qq.com>
2024-09-20 13:34:21 +0800

    2024.9.20

---
+++ LC1759.cpp
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    int countHomogenous(string s) {
+        long long ans=0;
+        int mod = 1e9+7;
+        for (int i = 0; i < s.size(); ++i) {
+            char c = s[i];
+            long long cnt=1;
+            while (i+1<s.size() && s[i]==s[i+1]){
+                ++cnt;
+                ++i;
+            }
+            ans = ans+(1+cnt)*cnt/2;
+            ans = ans%mod;
+        }
+        return (int)ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution();
+    return 0;
+}
