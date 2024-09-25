570bd65af074de221ee167e27279b569292b7a9c
swingthehook <513503877@qq.com>
2024-09-25 12:59:38 +0800

    2024.9.24

---
+++ LC2207.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node *> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node *> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    long long maximumSubsequenceCount(string text, string pattern) {
+        long long cnt1 = 0, cnt2 = 0;
+        vector<int> pre2(text.size());
+        for (int i = 0; i < text.size(); ++i) {
+            if (text[i] == pattern[0]) {
+                cnt1++;
+            }
+            if (text[i] == pattern[1]) {
+                cnt2++;
+            }
+            pre2[i] = cnt2;
+        }
+//        cout<<cnt1<<" "<<cnt2;
+        if(cnt1 == 0)
+            return cnt2;
+        if(cnt2==0)
+            return cnt1;
+        long long ans=0;
+        for (int i = 0; i < text.size(); ++i) {
+            if(text[i]==pattern[0]){
+                ans+=cnt2-pre2[i];
+            }
+        }
+        ans+= max(cnt1,cnt2);
+        return ans;
+    }
+};
+
+int main() {
+    int num = 2;
+    string s="vnedkpkkyxelxqptfwuzcjhqmwagvrglkeivowvbjdoyydnjrqrqejoyptzoklaxcjxbrrfmpdxckfjzahparhpanwqfjrpbslsyiwbldnpjqishlsuagevjmiyktgofvnyncizswldwnngnkifmaxbmospdeslxirofgqouaapfgltgqxdhurxljcepdpndqqgfwkfiqrwuwxfamciyweehktaegynfumwnhrgrhcluenpnoieqdivznrjljcotysnlylyswvdlkgsvrotavnkifwmnvgagjykxgwaimavqsxuitknmbxppgzfwtjdvegapcplreokicxcsbdrsyfpustpxxssnouifkypwqrywprjlyddrggkcglbgcrbihgpxxosmejchmzkydhquevpschkpyulqxgduqkqgwnsowxrmgqbmltrltzqmmpjilpfxocflpkwithsjlljxdygfvstvwqsyxlkknmgpppupgjvfgmxnwmvrfuwcrsadomyddazlonjyjdeswwznkaeaasyvurpgyvjsiltiykwquesfjmuswjlrphsdthmuqkrhynmqnfqdlwnwesdmiiqvcpingbcgcsvqmsmskesrajqwmgtdoktreqssutpudfykriqhblntfabspbeddpdkownehqszbmddizdgtqmobirwbopmoqzwydnpqnvkwadajbecmajilzkfwjnpfyamudpppuxhlcngkign";
+
+    vector<int> nums = {7, 12, 9, 8, 9, 15};
+    Solution().maximumSubsequenceCount(s,"rr");
+    return 0;
+}
