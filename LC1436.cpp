e76cf1d9ed75d6c392f76f5e07b5d25d4770ff3c
swingthehook <513503877@qq.com>
2024-10-08 08:35:33 +0800

    2024.10.8

---
+++ LC1436.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode() : val(0), next(nullptr) {}
+
+    ListNode(int x) : val(x), next(nullptr) {}
+
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_map<string,string> mp;
+        for(auto& each:paths){
+            mp[each[0]] = each[1];
+        }
+        auto str = paths[0][1];
+        while (mp.find(str)!=mp.end()){
+            str = mp[str];
+        }
+        return str;
+    }
+};
+
+int main() {
+    int num = 2;
+    vector<int> nums = {7,12,9,8,9,15};
+    Solution();
+    return 0;
+}
